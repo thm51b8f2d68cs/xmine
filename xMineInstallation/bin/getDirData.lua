a3fbe2e844ee1e958cdc79c21d5e830656ad4507
thm51b8f2d68cs <68766033+thm51b8f2d68cs@users.noreply.github.com>
2020-07-30 00:55:28 -0700

    Create getDirData.lua

---
+++ xMineInstallation/bin/getDirData.lua
@@ -0,0 +1,13 @@
+function main()
+  xdirdata = io.open("/.dirdata.txt", "r")
+  dataFromFile = xdirdata:read("*a")
+  print(dataFromFile)
+  dirdata = textutils.unserialize(dataFromFile)
+  print(dirdata)
+  print(dirdata.xroot)
+  os.sleep(5)
+  xdirdata:close()
+  return dirdata
+end
+ 
+main()
