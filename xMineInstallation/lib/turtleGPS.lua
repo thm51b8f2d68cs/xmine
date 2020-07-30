13e979534e08b4e576e1eda8b76119b3909e5803
thm51b8f2d68cs <68766033+thm51b8f2d68cs@users.noreply.github.com>
2020-07-30 00:58:20 -0700

    Create turtleGPS.lua

---
+++ xMineInstallation/lib/turtleGPS.lua
@@ -0,0 +1,43 @@
+turtleX = 0
+turtleZ = 0
+ 
+function setTurtlePos(x,z)
+  turtleX = x
+  turtleZ = z
+end
+ 
+function updateTurtleX(x)
+  turtleX = x
+  printTurtleX()
+  os.sleep(1)
+end
+ 
+function updateTurtleZ(z)
+  turtleZ = z
+  printTurtleZ()
+  os.sleep(1)
+end
+ 
+function getTurtlePos()
+  --return {"x" = turtleX, "z" = turtleZ}
+end
+ 
+function getTurtleX()
+  return turtleX
+end
+ 
+function getTurtleZ()
+  return turtleZ
+end
+ 
+function printTurtleX()
+  print(turtleX)
+end
+ 
+function printTurtleZ()
+  print(turtleZ)
+end
+ 
+function printTurtlePos()
+  print("X:", turtleX, "Z:", turtleZ)
+end
