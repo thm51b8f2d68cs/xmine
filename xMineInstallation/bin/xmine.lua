84cb66c8e6cbe1a4b2639ba11702c950bb0eb99a
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-01 04:34:54 -0700

    Adjusted shaft logging to an independent script. Added completion tracking to check if the turtle mined the shaft properly.
    Directories added: None.
    
    Files added: shaftLogger.lua
    
    Method added:
    isCoreShaftCompleted() - Checks if shaft has been mined down properly.

--- xMineInstallation/bin/xmine.lua
+++ xMineInstallation/bin/xmine.lua
@@ -4,6 +4,7 @@ xroot = xDirData.xroot
 xbin = xDirData.xbin
 xlib = xDirData.xlib
 xdata = xDirData.xdata
+excavationsPath = xroot .. xdata .. ".excavations.txt"
  
 function main()
   --os.loadAPI(xroot .. xlib .. "fs.lua")
@@ -40,9 +41,7 @@ end
 function autoSetup()
  
   --Read excavation file to see if a core shaft has already been dug in the vicinity of 500 blocks
-  path = xroot .. xdata .. ".excavations.txt"
-  createVectorsFromFile(path)
-  if (nearCoreShaft(gps.locate() ) ) then
+  if (not dofile(xroot .. xbin .. "shaftLogger.lua")) then
     runCoreShaft()
   else
     runQuadrantShaft()
@@ -50,55 +49,27 @@ function autoSetup()
 end
  
 function runCoreShaft()
-  shell.run(xroot .. xbin .. "excavateQuadrant.lua")
+  shell.run(xroot .. xbin .. "excavateCoreShaft.lua")
+  --Ask user if it ran correctly eg. did it excavate the desired dimensions
+  --or was there an error like the turtle did not finish
+  choice = gui.promptUser("Did your turtle excavate the core as desired?\n [Yes/No - there were errors]")
+  if (choice == "No") then
+    while (choice == "No") do
+      shell.run(xroot .. xbin .. "excavateCoreShaft.lua")
+      choice = gui.promptUser("Did your turtle excavate the core as desired?\n [Yes/No - there were errors]")
+    end
+  end
+  logCoreShaftExcavation()
 end
  
 function runQuadrantShaft()
-  shell.run(xroot .. xbin .. "excavateCoreShaft.lua")  
+  shell.run(xroot .. xbin .. "excavateQuadrant.lua"")
 end
- 
-function nearCoreShaft(x, y, z)
-  currentLocation = vector.new(x,y,z)
-  for index in pairs(vectors) do
-    if (currentLocation - index < 500) then
-      return true
-    end
-  end
-  return false
-end
- 
-function createVectorsFromFile(path)
-  vectors = {}
-  --excavations.txt should be formatted as
-  --x
-  --y
-  --z
-  --x
-  --y
-  --z
-  excavationData = io.open(path, "r")
-  lineNum = 1
-  index = 1
-  x = 0
-  y = 0
-  z = 0
-  for line in excavationData:lines() do
-    if (lineNum < 3) then
-      if (lineNum == 1) then
-        x = excavationData:read("*n")
-        lineNum = lineNum + 1
-      else
-        y = excavationData:read("*n")
-        lineNum = lineNum + 1
-      end
-    else
-      z = excavationData:read("*n")
-      lineNum = 1
-      vectors.index = vector.new(x,y,z)
-      index = index + 1
-    end
-  end
-  excavationData:close()
+
+function logCoreShaftExcavation()
+  excavations = io.open(excavationsPath, "a")
+  excavations:write(gps.locate())
+  excavations:close()
 end
  
 main()
