deac539ec05ade6025929ff27067e4c51fe8bb7b
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-02 21:28:51 -0700

    Removed debug prints in getDirData.lua

--- xMineInstallation/bin/getDirData.lua
+++ xMineInstallation/bin/getDirData.lua
@@ -1,11 +1,7 @@
 function main()
   xdirdata = io.open("/.dirdata.txt", "r")
   dataFromFile = xdirdata:read("*a")
-  print(dataFromFile)
   dirdata = textutils.unserialize(dataFromFile)
-  print(dirdata)
-  print(dirdata.xroot)
-  os.sleep(5)
   xdirdata:close()
   return dirdata
 end
